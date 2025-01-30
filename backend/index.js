74290f300ebf193ff4a35e0d86e26bbfb595039e
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-01-30 13:02:26 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 // index.js
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config(); 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
@@ -11,35 +11,58 @@ app.use(express.json());
 
 app.use(cors());
 
-const Task = require('./schemas/Task');
+const Task = require("./schemas/Task");
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Task Management API' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Task Management API" });
 });
 
-
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log('Connected to MongoDB Atlas');
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    })
-    .catch(err => {
-        console.error('Could not connect to MongoDB:', err);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB:", err);
+  });
 
-app.get('/tasks', async (req, res) => {
-    console.log('Fetching tasks...');
-    try {
-        const tasks = await Task.find().sort({ createdAt: -1 }); // Sort by newest first
-        res.json(tasks);
-    } catch (err) {
-        console.error('Error fetching tasks:', err);
-        res.status(500).json({ message: 'Server Error' });
-    }
+app.get("/tasks", async (req, res) => {
+  console.log("Fetching tasks...");
+  try {
+    const tasks = await Task.find().sort({ createdAt: -1 }); // Sort by newest first
+    res.json(tasks);
+  } catch (err) {
+    console.error("Error fetching tasks:", err);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
-
 
 // Write an endpoint to create a new task.
+app.post("/create-task", async (req, res) => {
+  const { title, dueDate, priority, status } = req.body;
 
+  if (!title || !dueDate || !priority || !status) {
+    return res
+      .status(401)
+      .send({ message: "all fields are required", success: true });
+  }
+
+  try {
+    const task = await Task.create({
+      title,
+      dueDate,
+      priority,
+      status,
+    });
+    console.log("task created");
+    return res
+      .status(200)
+      .send({ message: "task created", task, success: true });
+  } catch (err) {
+    console.error("Error creating task:", err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,10 +1,82 @@
 // src/components/TaskForm.jsx
 // You need to write the code for TaskForm component in the TaskForm.jsx file.
+import { useState } from "react";
+import axios from "axios";
 
-export default function TaskForm() {
+export default function TaskForm({onTaskCreated}) {
+    const [formData, setFormData] = useState({
+        title: "",
+        dueDate: "",
+        priority: "",
+        status: ""
+    })
+    const [error, setError] = useState("");
+
+    const styler = {
+        error:{
+            color: "red"
+        },
+        txt: {
+            color: "black"
+        },
+        inpBox: {
+            padding: "0.5rem"
+        }
+    }
+
+    const handleChange = (e)=>{
+        const name = e.target.name;
+        const val = e.target.value;
+        setFormData(prev=>({
+            ...prev,
+            [name]: val
+        }));
+    }
+    
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        if(formData.title=="" || formData.dueDate=="" || formData.priority=="" || formData.status==""){
+            setError("all the fields are required");
+            return;
+        }
+
+        try {
+            const res = await axios.post('http://localhost:3000/create-task', formData);
+            console.log('task created');
+            onTaskCreated();
+            setError("");
+        } catch (error) {
+            console.log(error.message);
+            setError(error.message);
+        }
+        
+    }
+
     return (
         <div>
-            {/* Write your code here */}
+            <div style={styler.error}>{error}</div>
+            <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection:'column'}}>
+                <label htmlFor="title" style={styler.txt}>Title</label>
+                <input type="text" style={styler.inpBox} name="title" value={formData.title} onChange={handleChange} />
+                <label htmlFor="dueDate" style={styler.txt}>Due Date</label>
+                <input type="date" style={styler.inpBox} name="dueDate" value={formData.dueDate} onChange={handleChange}/>
+                <label htmlFor="priority" style={styler.txt}>Priority</label>
+                <select name="priority"style={styler.inpBox}  value={formData.priority} onChange={handleChange}>
+                    <option value=""></option>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                </select>
+                <label htmlFor="status" style={styler.txt}>Status</label>
+                <select name="status" style={styler.inpBox} value={formData.status} onChange={handleChange}>
+                    <option value=""></option>
+                    <option value="To Do">To Do</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Done">Done</option>
+                </select>
+                <br />
+                <button onSubmit={handleSubmit}>Submit</button>
+            </form>
         </div>
     );
 }
